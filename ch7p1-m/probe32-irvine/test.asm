455ee20e4a7a6fbba4a8c02d209e110faff142f2
kylej19 <kmj19@tuta.io>
2026-04-22 19:53:12 -0500

    Refactor and expand test.asm exercises and output
    - Reorganize includes, .data section, and file description
    - Unify and clarify title/message strings
    - Replace msg1a/msg1b with single msg1 ("EAX= ")
    - Add crlf calls for better output formatting
    - Use CDQ for sign extension in division (Exercise 4)
    - Refactor Exercise 5: new section, improved buffer/array handling
    - Update packed2asc: use SHLD, null-terminate buffer
    - General cleanup and improved exercise separation

--- ch7p1-m/probe32-irvine/test.asm
+++ ch7p1-m/probe32-irvine/test.asm
@@ -1,22 +1,20 @@
 ; test.asm
 ; author: kyle johnson
-; description: template using irvine lib
+; description: chapter 7 assignment
 ;----------------------------------------
-
-INCLUDE irvine32.inc
-
-.data                               ;data
+include irvine32.inc
+.data                                           ;reserved address data
 ;----------------------------------------
-title1 BYTE "Exercise #1",0dh,0ah,"-------------",0dh,0ah,0
-title2 BYTE "Exercise #2",0dh,0ah,"-------------",0dh,0ah,0
-title3 BYTE "Exercise #3",0dh,0ah,"-------------",0dh,0ah,0
-title4 BYTE "Exercise #4",0dh,0ah,"-------------",0dh,0ah,0
-title5 BYTE "Exercise #5",0dh,0ah,"-------------",0dh,0ah,0
-msg1a BYTE "AX= ",0
-msg1b BYTE "EAX= ",0
+title1 BYTE 0dh,0ah,"Exercise #1",0dh,0ah,"-------------",0dh,0ah,0
+title2 BYTE 0dh,0ah,"Exercise #2",0dh,0ah,"-------------",0dh,0ah,0
+title3 BYTE 0dh,0ah,"Exercise #3",0dh,0ah,"-------------",0dh,0ah,0
+title4 BYTE 0dh,0ah,"Exercise #4",0dh,0ah,"-------------",0dh,0ah,0
+title5 BYTE 0dh,0ah,"Exercise #5",0dh,0ah,"-------------",0dh,0ah,0
+msg1 BYTE "EAX= ",0
 msg2 BYTE "AL= ",0
 msg3 BYTE "AX= ",0
 msg4 BYTE "(val2 / val3) * (val1 + val2)= ",0
+msg5 BYTE "Buffer=",0
 wVal1 WORD -5
 dVal1 DWORD -1
 dval2 DWORD -6
@@ -24,14 +22,14 @@ dval3 DWORD 3
 numArr DWORD 12345678h,23459876h,23848572h,96847321h
 buffer BYTE 9 dup(0)
 
-.code                               ;code
+.code                           ;executable code
 ;----------------------------------------
 main PROC
 ;             Exercise #1
 ;----------------------------------------
     mov edx,offset title1       ;output title
     call writestring
-    mov edx,offset msg1a        
+    mov edx,offset msg3        
     call writestring
     xor eax,eax                 ;clear out eax
     mov ebx,type word           ;for writehexb
@@ -43,7 +41,7 @@ main PROC
     sar eax,16                  ;shift ax down arithmetically (keep the sign)
 
     pushad                      ;store our registers
-    mov edx,offset msg1b        
+    mov edx,offset msg1        
     call writestring            ;display a message
     popad                       ;pop the registers we intend to display
 
@@ -72,6 +70,7 @@ E2:
     popad                       ;pop the registers we intend to display
 
     call writehexb              ;display al, again
+    call crlf
 
 ;             Exercise #3
 ;----------------------------------------
@@ -85,6 +84,7 @@ E2:
     imul ebx                        ;multiply eax by ebx
     call writeint                   ;display the result
     mov dVal1,eax                   ;store it in dVal1
+    call crlf
 
 ;             Exercise #4
 ;----------------------------------------
@@ -92,31 +92,37 @@ E2:
     call writestring                ;display the title
     mov edx,offset msg4
     call writestring                ;display a message
-    xor edx,edx                     ;clear the upper half of our dividend
+    cdq                             ;sign-extend the upper half of our dividend
     mov eax,dVal2                   ;place dVal2 into eax
     idiv dVal3                      ;divide by dVal3
     mov ebx,dVal1                   ;mov dVal1 into ebx
     add ebx,dVal2                   ;add dVal2 to ebx
     imul ebx                        ;multiply (dVal2/dVal3)*(dVal1+dVal2)
     call writeint                   ;display the product
-    mov dVal1,eax                   ;move our product into dVal1 
-
-    mov edx,offset title5
-    call writestring
+    mov dVal1,eax                   ;move our product into dVal1
     call crlf
 
+;             Exercise #5
+;----------------------------------------
+
+    mov edx,offset title5
+    call writestring                ;display the title for exercise 5
+    mov edx,offset msg5
+    call writestring                ;help display output
+    
     ;Exercise 5 calls
-    mov edx,offset buffer 
-    mov esi,offset numArr
-    mov eax,dword ptr(esi)
+    mov edi,offset buffer           ;move a pointer to our buffer in edx         
+    mov esi,offset numArr           ;point esi to our array of nums
+    mov edx,[esi]                   ;dereference the first dword val in nums
+
     call packed2asc
-    mov edx,offset buffer
-    call writestring
+    mov edx,offset buffer           ;place the offset of buffer in edx
+    call writestring                ;use writestring to write the buffer to stdout
+    call crlf       
 
     exit
 main ENDP
 
-;             Exercise #5
 ;----------------------------------------
 ; packed2asc - converts 4 byte packed decimal to ascii and displays
 ; input= edx:packed decimal binary doubleword,
@@ -126,12 +132,13 @@ packed2asc PROC
     mov ecx,lengthof buffer - 1
 L1:
     xor eax,eax                 ;clear out eax
-    shrd eax,edx,4              ;shift the upper nybble 
+    shld eax,edx,4              ;shift the upper nybble 
+    shl edx,4
     or al,30h
     mov byte ptr [edi],al
     add edi,type buffer
     loop L1
-
+    mov byte ptr [edi],0
     ret
 packed2asc ENDP
 
